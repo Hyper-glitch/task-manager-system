9c49605b8eeddc3e7486f5525af78f568498fd87
Roman Smirnov <graff049@gmail.com>
2024-02-24 19:36:07 +0300

    feat: add and rename settings values

--- auth-service/src/config.py
+++ auth-service/src/config.py
@@ -1,10 +1,16 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings
+
+from src.conf.amqp import AMQPConfigSettings
+from src.enums.producer import UserProducer
 
 
 class Settings(BaseSettings):
+    project: str = UserProducer.AUTH_SERVICE.value
     api_host: str
     api_port: int
-    sqlalchemy_database_uri: str
+    database_dsn: str
+    kafka: AMQPConfigSettings = Field(default_factory=AMQPConfigSettings)
 
     class Config:
         env_file = ".env"
