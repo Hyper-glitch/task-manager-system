f8ef15ddba880bc7baade23196bb3e834aa652d9
Roman Smirnov <graff049@gmail.com>
2024-02-24 20:11:41 +0300

    refactor: use dsn attr

--- auth-service/src/app.py
+++ auth-service/src/app.py
@@ -4,5 +4,5 @@ from src.config import settings
 from src.database import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = settings.sqlalchemy_database_uri
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.database_dsn
 db.init_app(app)
