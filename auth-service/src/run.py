99b4e92aaa768c88cd84c3465f7aa35ab0ef9439
Roman Smirnov <graff049@gmail.com>
2024-02-24 19:28:50 +0300

    refactor: run app

--- auth-service/src/run.py
+++ auth-service/src/run.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
+from src.app import app
 from src.config import settings
-from src.database import db
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = settings.sqlalchemy_database_uri
-    db.init_app(app)
     app.run(host=settings.api_host, port=settings.api_port)
