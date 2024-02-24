a2e9433acf4d09a9c6a668e19bba54301516298f
Roman Smirnov <graff049@gmail.com>
2024-02-24 20:11:31 +0300

    refactor: merge settings in one

--- auth-service/src/config.py
+++ auth-service/src/config.py
@@ -1,7 +1,5 @@
-from pydantic import Field
 from pydantic_settings import BaseSettings
 
-from src.conf.amqp import AMQPConfigSettings
 from src.enums.producer import UserProducer
 
 
@@ -10,10 +8,21 @@ class Settings(BaseSettings):
     api_host: str
     api_port: int
     database_dsn: str
-    kafka: AMQPConfigSettings = Field(default_factory=AMQPConfigSettings)
+
+    kafka_host: str
+    kafka_port: int
+    business_event_topic: str
+    data_streaming_topic: str
+    error_topic: str
+    reconnect_waiting: int
+    max_pool_interval_ms: int
 
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
+
+    @property
+    def bootstrap_servers(self) -> str:
+        return f"{self.host}:{self.port}"
 
 
 settings = Settings()
