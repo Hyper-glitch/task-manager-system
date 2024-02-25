b11083e415f4130e537d9d29904feef99b714f1f
Roman Smirnov <graff049@gmail.com>
2024-02-25 18:15:35 +0300

    feat: a lot of changes in views

--- auth-service/src/api/v1/user/views.py
+++ auth-service/src/api/v1/user/views.py
@@ -1,48 +1,59 @@
 from http import HTTPStatus
 
-from flask import Response, request
+from flask import Response
+from flask_restx import Resource, reqparse
 
-from src.app import app
+from src.dto.api.user import UserAddDTO
 from src.services import user_service
 from src.services.exceptions import UserNotFound, UserAlreadyExists
 from src.utils import make_response
 
 
-@app.route('/<int:user_id>', methods=['GET'])
-def get_user(user_id: int) -> Response:
-    try:
-        user = user_service.get_user(user_id=user_id)
-    except UserNotFound:
-        return Response(status=HTTPStatus.NOT_FOUND, response="User not found")
-    return make_response(code=200, **user.dict())
+class User(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str)
+    parser.add_argument('email', type=str)
+    parser.add_argument('role', type=str)
+    parser.add_argument('beak_shape', type=str)
+    parser.add_argument('user_id', type=str)
 
+    def get(self) -> Response:
+        data = User.parser.parse_args()
+        try:
+            user_dto = user_service.get_user(user_id=data["user_id"])
+        except UserNotFound:
+            return Response(status=HTTPStatus.NOT_FOUND, response="User not found")
+        else:
+            return make_response(code=200, **user_dto.model_dump())
 
-@app.route('/', methods=['POST'])
-def create_user() -> Response:
-    data = request.get_json()
-    try:
-        user = user_service.create_user(data=data)
-    except UserAlreadyExists:
-        return Response(status=HTTPStatus.CONFLICT, response="User already exists")
-    return make_response(code=200, **user.dict())
+    def post(self) -> Response:
+        data = User.parser.parse_args()
+        try:
+            dto = UserAddDTO.model_validate(data)
+            user_dto = user_service.create_user(dto=dto)
+        except UserAlreadyExists:
+            return Response(status=HTTPStatus.CONFLICT, response="User already exists")
+        except Exception as exc:
+            return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR, response=str(exc))
+        else:
+            return make_response(code=200, **user_dto.model_dump())
 
+    def put(self) -> Response:
+        data = User.parser.parse_args()
+        try:
+            user_dto = user_service.change_user_role(user_id=data["user_id"], role=data["role"])
+        except UserNotFound:
+            return Response(status=HTTPStatus.NOT_FOUND, response="User not found")
+        except Exception as exc:
+            return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR, response=str(exc))
+        return make_response(code=200, **user_dto.model_dump())
 
-@app.route('/<int:user_id>', methods=['PUT'])
-def update_user(
-        user_id: int,
-) -> Response:
-    data = request.get_json()
-    try:
-        user = user_service.change_user_role(user_id=user_id, role=data.role)
-    except UserNotFound:
-        return Response(status=HTTPStatus.NOT_FOUND, response="User not found")
-    return make_response(code=200, **user.dict())
-
-
-@app.route('/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id: int) -> Response:
-    try:
-        user = user_service.delete_user(user_id=user_id)
-    except UserNotFound:
-        return Response(status=HTTPStatus.NOT_FOUND, response="User not found")
-    return make_response(code=204, **user.dict())
+    def delete(self) -> Response:
+        data = User.parser.parse_args()
+        try:
+            user_dto = user_service.delete_user(user_id=data["user_id"])
+        except UserNotFound:
+            return Response(status=HTTPStatus.NOT_FOUND, response="User not found")
+        except Exception as exc:
+            return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR, response=str(exc))
+        return make_response(code=204, **user_dto.model_dump())
