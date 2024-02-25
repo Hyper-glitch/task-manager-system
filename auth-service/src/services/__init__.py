9144c4a8fb38bdf5f759399595f8fc18911805e3
Roman Smirnov <graff049@gmail.com>
2024-02-25 18:51:59 +0300

    feat: create repo and services in init

--- auth-service/src/services/__init__.py
+++ auth-service/src/services/__init__.py
@@ -1,3 +1,8 @@
+from src.database import db
+from src.repositories.user import UserRepository
+from src.services.oauth import OAuthService
 from src.services.user import UserService
 
-user_service = UserService()
+repository = UserRepository(session=db.session)
+user_service = UserService(repository=repository)
+oauth_service = OAuthService(repository=repository)
