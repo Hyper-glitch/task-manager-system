0a4415304852e41e8fdf1066efa6562311c396b5
Roman Smirnov <graff049@gmail.com>
2024-02-25 18:09:03 +0300

    feat: add create app

--- auth-service/src/app.py
+++ auth-service/src/app.py
@@ -3,6 +3,10 @@ from flask import Flask
 from src.config import settings
 from src.database import db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = settings.database_dsn
-db.init_app(app)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = settings.database_dsn
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    return app
