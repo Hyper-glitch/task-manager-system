5e591579139dfc7b9360eda774788b9ed67c2ce9
Roman Smirnov <graff049@gmail.com>
2024-02-25 18:10:25 +0300

    feat: add uuid type remove: optional refactor: rename dto

--- auth-service/src/dto/events/user.py
+++ auth-service/src/dto/events/user.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 from datetime import datetime
 from typing import Optional
+from uuid import UUID
 
 from pydantic import (
     BaseModel,
-    Field,
+    Field, Extra,
 )
 
 from src.enums.events import EventTitleUser
@@ -13,38 +14,41 @@ from src.enums.producer import UserProducer
 from src.enums.roles import UserRoles
 
 
-class UserDataDTO(BaseModel):
-    public_id: str = Field(..., title="Public Id")
+class UserEventDTO(BaseModel, extra=Extra.allow):
+    public_id: UUID = Field(..., title="Public Id")
     username: str = Field(..., title="Username")
     email: str = Field(..., title="Email")
     role: UserRoles
 
+    class Config:
+        from_attributes = True
+
 
 class UserRoleChangedDataDTO(BaseModel):
-    public_id: str = Field(..., title="Public Id")
+    public_id: UUID = Field(..., title="Public Id")
     old_role: Optional[UserRoles] = None
     new_role: UserRoles
 
 
 class UserCreatedEventDTO(BaseModel):
-    version: Optional[int] = Field(1, title="Version")
-    produced_at: Optional[datetime] = Field(None, title="Produced At")
+    version: int = Field(1, title="Version")
+    produced_at: datetime = Field(None, title="Produced At")
     title: EventTitleUser = EventTitleUser.USER_CREATED
-    data: UserDataDTO
+    data: UserEventDTO
     producer: UserProducer = UserProducer.AUTH_SERVICE
 
 
 class UserRoleChangedEventDTO(BaseModel):
-    version: Optional[int] = Field(1, title="Version")
-    produced_at: Optional[datetime] = Field(None, title="Produced At")
+    version: int = Field(1, title="Version")
+    produced_at: datetime = Field(None, title="Produced At")
     title: EventTitleUser = EventTitleUser.USER_ROLE_CHANGED
     data: UserRoleChangedDataDTO
     producer: UserProducer = UserProducer.AUTH_SERVICE
 
 
 class UserDeletedEventDTO(BaseModel):
-    version: Optional[int] = Field(1, title="Version")
-    produced_at: Optional[datetime] = Field(None, title="Produced At")
+    version: int = Field(1, title="Version")
+    produced_at: datetime = Field(None, title="Produced At")
     title: EventTitleUser = EventTitleUser.USER_DELETED
-    data: UserDataDTO
+    data: UserEventDTO
     producer: UserProducer = UserProducer.AUTH_SERVICE
