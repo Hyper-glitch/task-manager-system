5548e481fee89e1f625861fe25add19755671e01
Roman Smirnov <graff049@gmail.com>
2024-02-25 18:53:03 +0300

    feat: add basic oauth resource

--- auth-service/src/run.py
+++ auth-service/src/run.py
@@ -1,6 +1,8 @@
 from flask_restx import Api
 
-from src.api.v1.user.views import User
+
+from src.api.v1.oauth import OAuth
+from src.api.v1.users import User
 from src.app import create_app
 from src.config import settings
 from src.database import db
@@ -11,6 +13,7 @@ app = create_app()
 def add_resource():
     api = Api(app)
     api.add_resource(User, '/user')
+    api.add_resource(OAuth, '/authorize')
 
 
 @app.before_request
