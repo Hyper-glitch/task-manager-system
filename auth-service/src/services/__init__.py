0bbd74f3e79cfbeec88849f3b908658cd4886b38
Roman Smirnov <graff049@gmail.com>
2024-02-26 19:36:41 +0300

    feat: add redis for services

--- auth-service/src/services/__init__.py
+++ auth-service/src/services/__init__.py
@@ -1,8 +1,11 @@
 from src.database import db
+from src.redis.manager import RedisManager
 from src.repositories.user import UserRepository
 from src.services.oauth import OAuthService
 from src.services.user import UserService
 
+redis_manager = RedisManager()
+
 repository = UserRepository(session=db.session)
-user_service = UserService(repository=repository)
-oauth_service = OAuthService(repository=repository)
+user_service = UserService(repository=repository, redis=redis_manager.redis)
+oauth_service = OAuthService(repository=repository, redis=redis_manager.redis)
