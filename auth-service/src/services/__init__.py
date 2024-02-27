7c57dbcb0a2014870f1db209bc13341a7df65947
Roman Smirnov <graff049@gmail.com>
2024-02-27 19:05:21 +0300

    feat: add user_refresh_token_repo

--- auth-service/src/services/__init__.py
+++ auth-service/src/services/__init__.py
@@ -1,11 +1,13 @@
 from src.database import db
 from src.redis.manager import RedisManager
 from src.repositories.user import UserRepository
+from src.repositories.user_token import UserRefreshTokenRepo
 from src.services.oauth import OAuthService
 from src.services.user import UserService
 
 redis_manager = RedisManager()
 
-repository = UserRepository(session=db.session)
-user_service = UserService(repository=repository, redis=redis_manager.redis)
-oauth_service = OAuthService(repository=repository, redis=redis_manager.redis)
+user_repo = UserRepository(session=db.session)
+user_refresh_token_repo = UserRefreshTokenRepo(session=db.session)
+user_service = UserService(repository=user_repo, redis=redis_manager.redis)
+oauth_service = OAuthService(repository=user_refresh_token_repo, redis=redis_manager.redis)
