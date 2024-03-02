8093d6e43cd48c91054ceae1564c98f32083ed63
Roman Smirnov <graff049@gmail.com>
2024-03-02 23:59:43 +0300

    feat: add completed status

--- task-service/src/enums/events.py
+++ task-service/src/enums/events.py
@@ -4,4 +4,4 @@ from enum import Enum
 class EventTitleTaskCreated(str, Enum):
     CREATED = "TASK.CREATED"
     ASSIGNED = "TASK.ASSIGNED"
-    UPDATED = "TASK.UPDATED"
+    COMPLETED = "TASK.COMPLETED"
