5980bc14fe8314969c08e3f377d9ff7081ff8df9
Roman Smirnov <graff049@gmail.com>
2024-03-02 21:18:02 +0300

    feat: add user model

---
+++ task-service/src/models/user.py
@@ -0,0 +1,23 @@
+import uuid
+
+from sqlalchemy import Enum
+from sqlalchemy.dialects.postgresql import UUID
+
+from src.database import db
+from src.enums.roles import UserRoles
+from src.models.base import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    username = db.Column(db.String(128), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
+    role = db.Column(
+        Enum(UserRoles),
+        nullable=False,
+        default=UserRoles.EMPLOYEE,
+        server_default=db.text(f"'{UserRoles.EMPLOYEE.value}'"),
+    )
+    is_deleted = db.Column(db.Boolean)
