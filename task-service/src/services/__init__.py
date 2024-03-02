7386470d8e84c297c72de21b09cd3afb1978bb63
Roman Smirnov <graff049@gmail.com>
2024-03-02 23:39:26 +0300

    feat: init broker and add to task service

--- task-service/src/services/__init__.py
+++ task-service/src/services/__init__.py
@@ -1,12 +1,14 @@
 from src.database import db
+from src.kafka.manager import KafkaManager
 from src.repositories.task import TaskRepository
 from src.repositories.user import UserRepository
 from src.services.task import TaskService
 from src.services.user import UserService
 
+kafka = KafkaManager()
 
 user_repo = UserRepository(session=db.session)
 user_service = UserService(repository=user_repo)
 
 task_repo = TaskRepository(session=db.session)
-task_service = TaskService(repository=task_repo)
+task_service = TaskService(task_repo=task_repo, user_repo=user_repo, kafka=kafka)
