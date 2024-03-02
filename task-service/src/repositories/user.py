09139e70b5832e752ebe521738f3d40cdc09a92e
Roman Smirnov <graff049@gmail.com>
2024-03-02 23:42:03 +0300

    feat: add get_random_employees to repo

--- task-service/src/repositories/user.py
+++ task-service/src/repositories/user.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from typing import Any
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
+from src.enums.roles import UserRoles
 from src.models.user import User
 
 
@@ -14,3 +17,14 @@ class UserRepository:
 
     def get_by_public_id(self, public_id: str):
         return self._session.query(User).filter(User.public_id == public_id).first()
+
+    def get_random_employees(
+        self, count: int = 1, lock: bool = False, **lock_params: Any
+    ):
+        query = self._session.query(User)
+        if lock:
+            query = query.with_for_update(**lock_params)
+        return query.filter(
+                User.role == UserRoles.EMPLOYEE,
+                User.is_deleted.is_(False),
+            ).order_by(func.random()).limit(count)
