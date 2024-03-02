701fe196a9f26543a2dbe59c092559b944a95c11
Roman Smirnov <graff049@gmail.com>
2024-03-03 00:00:55 +0300

    feat: add complete_task to service

--- task-service/src/services/task.py
+++ task-service/src/services/task.py
@@ -3,8 +3,10 @@ from typing import Any
 
 from src.config import settings
 from src.dto.api.task import TaskDTO
-from src.dto.events.task import TaskCreatedEventDTO, TaskEventDTO, TaskAssignedEventSchema, TaskNewAssigneeDTO
+from src.dto.events.task import TaskCreatedEventDTO, TaskEventDTO, TaskNewAssigneeDTO, \
+    TaskCompletedEventDTO, TaskStatusChangedDataDTO, TaskAssignedEventDTO
 from src.kafka.manager import KafkaManager
+from src.models.task import Task
 from src.repositories.task import TaskRepository
 from src.repositories.user import UserRepository
 from src.services.exceptions import TaskNotFound
@@ -34,11 +36,10 @@ class TaskService:
         data_event = TaskCreatedEventDTO(
             data=TaskEventDTO.from_orm(task),
             produced_at=datetime.utcnow(),
-            version=2,
         )
         self.kafka.send(value=data_event.model_dump(mode="json"), topic=settings.data_streaming_topic)
 
-        business_event = TaskAssignedEventSchema(
+        business_event = TaskAssignedEventDTO(
             data=TaskNewAssigneeDTO(
                 public_id=task.public_id,
                 old_assignee_public_id=old_assignee_public_id,
@@ -48,3 +49,24 @@ class TaskService:
         )
         self.kafka.send(value=business_event.model_dump(mode="json"), topic=settings.business_event_topic)
         return TaskDTO.from_orm(task)
+
+    def complete_task(self, task_id: int) -> TaskDTO:
+        task = self.task_repo.get_by_id(task_id, lock=True, of=Task)
+        if not task:
+            raise TaskNotFound(f"Task {task_id} not found")
+
+        old_status = task.status
+        self.task_repo.status_done(task=task)
+
+        business_event = TaskCompletedEventDTO(
+            data=TaskStatusChangedDataDTO(
+                public_id=task.public_id,
+                assignee_public_id=task.assignee.public_id,
+                old_status=old_status,
+                new_status=task.status,
+            ),
+            produced_at=datetime.utcnow(),
+        )
+        self.kafka.send(value=business_event.model_dump(mode="json"), topic=settings.business_event_topic)
+        task_dto = TaskDTO.from_orm(task)
+        return task_dto
