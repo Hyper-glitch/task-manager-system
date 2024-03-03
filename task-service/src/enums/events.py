b6a8c2f4f994ec3e5968fde899a1d21c28096467
Roman Smirnov <graff049@gmail.com>
2024-03-03 12:47:58 +0300

    feat: add enum

--- task-service/src/enums/events.py
+++ task-service/src/enums/events.py
@@ -5,3 +5,10 @@ class EventTitleTaskCreated(str, Enum):
     CREATED = "TASK.CREATED"
     ASSIGNED = "TASK.ASSIGNED"
     COMPLETED = "TASK.COMPLETED"
+
+
+class EventTitleUser(str, Enum):
+    USER_CREATED = "USER.CREATED"
+    USER_ROLE_CHANGED = "USER.ROLE_CHANGED"
+    USER_DELETED = "USER.DELETED"
+    USER_UPDATED = "USER.UPDATED"
