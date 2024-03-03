408ef38516a4970ea5dcff1c59b6ac5f19ab7ef0
Roman Smirnov <graff049@gmail.com>
2024-03-03 12:49:09 +0300

    feat: add event_data_fabric

--- task-service/src/utils.py
+++ task-service/src/utils.py
@@ -4,6 +4,13 @@ from flask import Response, jsonify
 import jwt
 
 from src.config import settings
+from src.dto.events.user import UserCreatedEventDTO, UserRoleChangedEventDTO, UserDeletedEventDTO
+
+USER_EVENTS_MAP = {
+    ("USER.CREATED", 1): UserCreatedEventDTO,
+    ("USER.ROLE_CHANGED", 1): UserRoleChangedEventDTO,
+    ("USER.DELETED", 1): UserDeletedEventDTO,
+}
 
 
 def decode_token(token: str) -> dict[str, Any]:
@@ -24,3 +31,14 @@ def make_response(code: int, **data) -> Response:
     if data:
         response["data"] = data
     return jsonify(response)
+
+
+def event_data_fabric(data: dict[str, Any]) -> Any:
+    title = data.get("title")
+    version = data.get("version")
+    schema = USER_EVENTS_MAP.get((title, version))
+
+    if not schema:
+        raise ValueError(f"Unknown event: {data}")
+
+    return schema(**data)
