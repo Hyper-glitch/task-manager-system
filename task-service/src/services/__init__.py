1f4bd45fec0308c8dad50c34a7ac9dbd0da09767
Roman Smirnov <graff049@gmail.com>
2024-03-03 12:46:13 +0300

    feat: init user_event_service

--- task-service/src/services/__init__.py
+++ task-service/src/services/__init__.py
@@ -4,6 +4,7 @@ from src.repositories.task import TaskRepository
 from src.repositories.user import UserRepository
 from src.services.task import TaskService
 from src.services.user import UserService
+from src.services.user_events import UserEventService
 
 kafka = KafkaManager()
 
@@ -12,3 +13,5 @@ user_service = UserService(repository=user_repo)
 
 task_repo = TaskRepository(session=db.session)
 task_service = TaskService(task_repo=task_repo, user_repo=user_repo, kafka=kafka)
+
+user_event_service = UserEventService(user_repo=user_repo)
