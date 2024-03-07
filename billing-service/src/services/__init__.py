b082345dd65dc8a962e2f2acfeb11c02936f8cd2
Roman Smirnov <graff049@gmail.com>
2024-03-07 23:06:16 +0300

    feat: init acc service

--- billing-service/src/services/__init__.py
+++ billing-service/src/services/__init__.py
@@ -1,6 +1,11 @@
 from src.database import db
+from src.repositories.account import AccountRepository
 from src.repositories.billing import BillingCycleRepository
+from src.services.account import AccountService
 from src.services.billing import BillingCycleService
 
 billing_repo = BillingCycleRepository(session=db.session)
 billing_service = BillingCycleService(billing_repo=billing_repo)
+
+acc_repo = AccountRepository(session=db.session)
+acc_service = AccountService(acc_repo=acc_repo)
