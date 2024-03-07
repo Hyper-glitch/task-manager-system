ab080fbc4b5851ca8bc8f974403d2da898d010fa
Roman Smirnov <graff049@gmail.com>
2024-03-07 22:41:06 +0300

    feat: add task cost model

---
+++ billing-service/src/models/task.py
@@ -0,0 +1,18 @@
+import uuid
+
+from sqlalchemy.dialects.postgresql import UUID
+
+from src.database import db
+from src.models.base import Base
+
+
+class TaskCost(Base):
+    __tablename__ = "task_cost"
+
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
+    task_id = db.Column(db.String(128))
+    title = db.Column(db.String(128), nullable=False, default="", server_default=db.text("''"))
+    description = db.Column(db.Text, nullable=False, default="", server_default=db.text("''"))
+    credit_cost = db.Column("credit_cost", db.Integer, nullable=False)
+    debit_cost = db.Column("debit_cost", db.Integer, nullable=False)
