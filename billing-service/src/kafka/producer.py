6c396fd13ff9a0f005095ab6a6aa7cf54ac0517a
Roman Smirnov <graff049@gmail.com>
2024-03-10 15:03:17 +0300

    feat: add events producer

--- billing-service/src/kafka/producer.py
+++ billing-service/src/kafka/producer.py
@@ -1,12 +1,30 @@
+import logging
+
 from confluent_kafka import Producer
 
-# Create asynchronous Kafka producer
-producer = Producer({"bootstrap.servers": "localhost:9092"})
+from src.config import settings
 
-# Publish messages to topics asynchronously
-producer.produce("topic1", b"Hello from topic1", callback=delivery_callback)
-producer.produce("topic2", b"Hello from topic2", callback=delivery_callback)
+logger = logging.getLogger(settings.project)
 
 
-# Flush the producer's message queue
-producer.flush()
+class EventsProducer:
+    def __init__(self) -> None:
+        self.producer = Producer(
+            {
+                "bootstrap.servers": settings.bootstrap_servers,
+                "group.id": settings.group_id,
+            }
+        )
+
+    def send(self, value: str | bytes, topic: str) -> None:
+        try:
+            self.producer.produce(topic=topic, value=value)
+        except Exception as exc:
+            logger.error(
+                f"Cannot send payload to `{topic}` kafka topic. Reason: {exc}"
+            )
+        else:
+            logger.info(
+                f"Publishing to topic {topic}, message {value}"
+            )
+            self.producer.flush()
