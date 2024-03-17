20e0278994f9b1bf928be722fd528255ac2955ba
Roman Smirnov <graff049@gmail.com>
2024-03-17 11:52:40 +0300

    feat: registrate services

---
+++ analytics-service/src/services/__init__.py
@@ -0,0 +1,6 @@
+from src.services.balance import BalanceService
+from src.database import db
+from src.services.task_cost import TaskCostService
+
+balance_service = BalanceService(db.session)
+task_cost_service = TaskCostService(db.session)
